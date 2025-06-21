14ca739a6c08281478dc5d930f86a464cdb62d10
Harman Singh Hira <HSinghHira@users.noreply.github.com>
2025-06-22 11:39:33 +1200

    Building & Deploying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './.vitepress/**/*.{vue,js,ts,mjs}',
-    './.vitepress/theme/**/*.{vue,js,ts}',
-    './.vitepress/components/**/*.{vue,js,ts}',
+    './.vitepress/**/*.{vue,js,ts}',
     './en/**/*.{md,vue}',
     './pb/**/*.{md,vue}',
-    './**/*.md',
-    './index.md'
+    "**/*.md", 
+    
   ],
-  darkMode: 'class',
   theme: {
     extend: {},
   },
   plugins: [],
-  corePlugins: {
-    // Disable some conflicting plugins that might interfere with VitePress
-    preflight: false,
-  }
 };
